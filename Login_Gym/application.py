baf102c270bdef17e4cbbfa4dea27b16744d8f34
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-19 16:27:19 +0430

    40%

--- Login_Gym/application.py
+++ Login_Gym/application.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template ,url_for 
-
+from flask import Flask, render_template ,url_for,session 
+from flask_session import Session
+import sqlite3 
 
 app = Flask(__name__)
+
+# config session 
+app.config['SESSION_PERMANENT'] = True
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/singin')
+def singin():
+    return render_template('singin.html')
