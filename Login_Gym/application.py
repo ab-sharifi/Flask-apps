5466d63b0ee473b71e5dd75cd5af89d7e3a9a305
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-19 21:56:13 +0430

    Update application.py

--- Login_Gym/application.py
+++ Login_Gym/application.py
@@ -6,7 +6,6 @@ import math
 # create connection with data base
 db = SQL('sqlite:///database.db')
 
-
 app = Flask(__name__)
 
 # config session 
@@ -14,14 +13,10 @@ app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
-
-
 @app.route('/')
 def index():
 
     return render_template('index.html')
-
-
 
 @app.route('/login',methods=['POST','GET'])
 def login():
@@ -47,10 +42,6 @@ def login():
                 return render_template('login.html',message='Password is Wring')
         else:
             return render_template('login.html',message='Username Not found')
-
-
-
-
 
 @app.route('/singin',methods=['POST','GET'])
 def singin():
@@ -83,18 +74,13 @@ def singin():
     else:
         answer = db.execute("""INSERT INTO users ('username', 'password','email')
         VALUES (?,?,?) """,username,hash(password),email)
-
-        session["name"] = hash(password)
-        
+        session["name"] = hash(password)        
         return render_template('singin.html',success= 'Register complete')      
-
-
 
 @app.route('/logout')
 def logout():
     session["name"] = None
     return redirect('/')
-
 
 
 @app.route('/homePage')
