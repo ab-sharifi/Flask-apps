9689a89f5b5e20ef06d9b7a7675e9493168c8528
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-19 18:19:47 +0430

    70%

--- Login_Gym/application.py
+++ Login_Gym/application.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request ,url_for,session
 from flask_session import Session
 import sqlite3 
 
+# create connection with data base
+conn = sqlite3.connect('database.db')
+
+# create a cursor for Data base
+db = conn.cursor()
+
 app = Flask(__name__)
 
 # config session 
@@ -14,6 +20,7 @@ Session(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 
 @app.route('/login',methods=['POST','GET'])
@@ -29,14 +36,37 @@ def singin():
     if request.method == 'GET':
         return render_template('singin.html')
 
+    username = ''
+    password = ''
+    password_re = ''
+    email = ''
     # POST
     if request.method == 'POST':
-        username =request.form.get('username')
-        password =request.form.get('password')
-        password_re =request.form.get('password-re')
-
-    if not username or not password or not password_re:
+        username = request.form.get('username')
+        password = request.form.get('password')
+        password_re = request.form.get('password_re')
+        email = request.form.get('email')
+    
+    # check to all field is Not empty
+    if not username or not password or not password_re or not email:
         return render_template('singin.html',message='Please Complete Field ')
+    
+    # check if passwords are not match
     if password != password_re:
         return render_template('singin.html',message="Password's not Match! ")
-        
+    
+    # check is Username is available in data base or Not
+    answer = db.execute('SELECT username FROM users WHERE name LIKE ?',username)
+    conn.commit()
+    if len(answer) != 0 :
+        conn.close()
+        return render_template('singin.html',message='This Username takes bu another User')
+    
+    # if username is not in data base so we register user
+    else:
+        answer = db.execute("""INSERT INTO users ('username', 'password','email')
+        VALUES (?,?,?) """,username,hash(password),email)
+        conn.commit()
+        conn.close()
+        return render_template('singin.html',success= 'Register complete')      
+
