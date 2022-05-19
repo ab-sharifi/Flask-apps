6a57a2a6a94d9f449c600e98984ce33129be4d61
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-19 21:55:12 +0430

    Update application.py

--- Login_Gym/application.py
+++ Login_Gym/application.py
@@ -10,7 +10,7 @@ db = SQL('sqlite:///database.db')
 app = Flask(__name__)
 
 # config session 
-app.config['SESSION_PERMANENT'] = True
+app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
