e55c2921c1d29afe6a70dc392f08bc428916c5d3
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-25 23:00:06 +0430

    added file

--- Login-Page/app.py
+++ Login-Page/app.py
@@ -1,10 +1,45 @@
-import sqlite3
 from flask import Flask, render_template, redirect, request,session
+from cs50 import SQL
 
-def create_conn():
-    conn = sqlite3.connect('data.db')
-    return conn
+ 
+db=SQL('sqlite:///data.db')
+
+def validate(username):
+    # username cannot start with number
+    if username[0].isdigit():
+        return False
+
+    # check for sql injection 
+    if '-' in username:
+        return False
+
+    # check for username created by number and alphabet
+    for i in username:
+        if not i.isnumeric() and not i.isalpha():
+            return False
     
+    return True
+
+
+def validate_username(username):
+    answer = db.execute("""
+    SELECT username FROM users WHERE username LIKE ?""",(username))
+    
+    if len(answer) != 0:
+        return False
+    return True
+
+
+
+def create_account(username,password,email):
+    password =hash(password)
+    ans=db.execute("""
+    INSERT INTO users (username, password, email) VALUES (? ,? ,?)""",username,password,email)
+    if not ans:
+        return False
+    return True
+
+
 
 app = Flask(__name__)
 
@@ -29,9 +64,22 @@ def singup():
         # check box's Not Empty
         if not email or not username or not password:
             return render_template("singup.html",message='Error: Please Full The Box')
+        
+        # validate username is correct 
+        if (not validate(username)):
+            return render_template("singup.html",message='Username is Not Correct Username Cannot Start With Number or have Space Between it Username Should just have Number and Character')
+        
+
+        # validate username is not duplicate
+        if (not validate_username(username)):
+            return render_template("singup.html",message='Username Already Taked by another User :( ')
+
+        # create user account
+        if not create_account(email=email,username=username,password=password):
+            return render_template("singup.html",message='Error Something Wring In Server Side :( ')
 
         return render_template("singup.html",success='Register Complete')
-
+        
     
 
 
