c206ed41623afb983d92d7392c932a825ac25bee
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-06-04 00:01:36 +0430

    Update app.py

--- 80%/app.py
+++ 80%/app.py
@@ -3,9 +3,9 @@ import os
 
 from datetime import datetime
 
-from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect,jsonify, render_template, request, session
 from flask_session import Session
+from cs50 import SQL
 from tempfile import mkdtemp
 import requests
 from werkzeug.security import check_password_hash, generate_password_hash
